5b1ce58220898ad257020f7bdcecb49b2b871284
Jiwoo <78391069+yuziwoo@users.noreply.github.com>
2022-06-08 16:59:40 +0900

    blob 추가

--- 기본 고정/common.js
+++ 기본 고정/common.js
@@ -1,13 +1,43 @@
-window.addEventListener("resize", () => {
-  // Resize Error Solution
-  _dcode_resize();
-})
-
 window.addEventListener("DOMContentLoaded", () => {
-  // pc로 켜진 html은 1360px 아래로 떨어지지 않는다.
   if (document.body.offsetWidth < 1360) {
     document.body.classList.add("ver_mob");
   } else {
     document.body.classList.add("ver_pc");
   }
+
+  let scroll_effect = document.getElementsByClassName("scroll_effect");
+  for (let i=0; i < scroll_effect.length; i++) {
+    scroll_effect[i].classList.add("scroll_effect_off");
+  }
+
+  let scroll_function = (x, y, w, h) => {
+    try {
+      if (document.getElementsByClassName("scroll_effect_off")) {
+        for (let i=0; i < document.getElementsByClassName("scroll_effect_off").length; i++) {
+          if (document.getElementsByClassName("scroll_effect_off")[i].getBoundingClientRect().top < h / 5 * 3 + y && !document.getElementsByClassName("scroll_effect_off")[i].classList.contains("scroll_effect_on")) {
+              document.getElementsByClassName("scroll_effect_off")[i].classList.add("scroll_effect_on");
+              document.getElementsByClassName("scroll_effect_off")[i].classList.remove("scroll_effect_off");
+          }
+        }
+      }
+    } catch (exception) {
+      for (let i=0; i < document.getElementsByClassName("scroll_effect_off").length; i++) {
+        if (!document.getElementsByClassName("scroll_effect_off")[i].classList.contains("scroll_effect_on")) {
+            document.getElementsByClassName("scroll_effect_off")[i].classList.add("scroll_effect_on");
+            document.getElementsByClassName("scroll_effect_off")[i].classList.remove("scroll_effect_off");
+        }
+      }
+    }
+  }
+
+  window.itsdcode_onscroll = (x, y, w, h) => {
+    scroll_function(x, y, w, h);
+  }
+
+  window.itsdcode_onresize = (x, y, w, h) => {
+    _dcode_resize();
+    scroll_function(x, y, w, h);
+  }
+
 })
+
